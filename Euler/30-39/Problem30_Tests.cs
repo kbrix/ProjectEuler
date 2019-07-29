dadaa42406ad90d480d14edf7a202360553cf88e
Kristoffer Brix <kristoffer.brix@keylane.com>
2019-07-29 12:33:02 +0200

    WIP

---
+++ Euler/30-39/Problem30_Tests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace euler
+{
+    public class Problem30_Tests
+    {
+        [Fact]
+        public void ReturnsSolution()
+        {
+            var result = Problem30.Solution(4);
+            Assert.Equal(1, result);
+        }
+    }
+}
