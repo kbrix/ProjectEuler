5607e846dbeea3a0b3ccc310017c9032d39cd87e
Kristoffer Brix <kbrix@protonmail.com>
2019-11-23 17:34:01 +0100

    Solved problem 36 (Double-base palindromes)

---
+++ Euler/30-39/Problem36_Test.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace euler
+{
+    public class Problem36_Test
+    {
+        [Fact]
+        public void ReturnSolution()
+        {
+            Assert.Equal(872187, Problem36.Solution());
+        }
+    }
+}
