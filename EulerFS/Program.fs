1ab9194526168047001f89af8970e892542ea198
Kristoffer Brix <kristoffer.brix@keylane.com>
2021-01-31 12:22:18 +0100

    Oopsie, hehe

--- EulerFS/Program.fs
+++ EulerFS/Program.fs
@@ -101,7 +101,7 @@ let main argv =
                 | Some(67) -> solutionPrinter "Problem 67: 'Maximum path sum II'."  SolutionCS.Problem67.Solution
 
                 | Some(69) -> solutionPrinter "Problem 69: 'Totient maximum'."  (fun _ -> SolutionCS.Problem69.Solution 1_000_000)
-                | Some(70) -> solutionPrinter "Problem 69: 'Totient permutation'."  (fun _ -> SolutionCS.Problem70.Solution 10_000_000)
+                | Some(70) -> solutionPrinter "Problem 70: 'Totient permutation'."  (fun _ -> SolutionCS.Problem70.Solution 10_000_000)
                 
                 | Some(641) -> solutionPrinter "Problem 641: 'A Long Row of Dice'."  SolutionCS.Problem641.Solution
                 
