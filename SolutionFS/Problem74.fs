f3d936e78bf9d60956d64ba1fa5c28bb46854ff2
Kristoffer Brix <kristoffer.brix@keylane.com>
2021-03-29 09:05:55 +0200

    Solved problem 74

---
+++ SolutionFS/Problem74.fs
@@ -0,0 +1,44 @@
+﻿namespace Solution
+
+module Problem74 =
+
+    let factorial (n : bigint) =
+        if n = 0I || n = 1I then 1I
+        else [|1I .. n|] |> Array.reduce (*)
+
+    let fastFactorial n =
+        match n with
+        | 0 | 1 -> 1 
+        | 2     -> 2
+        | 3     -> 6
+        | 4     -> 24
+        | 5     -> 120
+        | 6     -> 720
+        | 7     -> 5040
+        | 8     -> 40320
+        | 9     -> 362880
+        | _     -> raise(System.ArgumentOutOfRangeException())
+
+    let factorialDigitSum (n : int) =
+        SolutionCS.Utility.Miscellaneous.Digits(n)
+            |> Seq.map (fastFactorial) 
+            |> Seq.sum
+
+    let factorialDigitSumLoopLength n =
+        let rec f (x : int) (cache : Set<int>) =
+            if cache |> Set.contains x then 
+                cache
+            else 
+                let updatedCache = Set.add x cache
+                let element = factorialDigitSum x
+                f element updatedCache
+        
+        let elements = f n Set.empty
+        elements |> Set.count
+
+    // a better solution would also cache the loops
+    let solution () = 
+        [|1 .. 1_000_000|]
+        |> Array.Parallel.map (factorialDigitSumLoopLength)
+        |> Array.filter (fun l -> l = 60)
+        |> Array.length
---
+++ SolutionTestsFS/Problem74Tests.fs
@@ -0,0 +1,28 @@
+﻿module Problem74Tests
+
+open NUnit.Framework
+
+[<Test>]
+let factorial_largeNumber_returnsFactorial () =
+    Assert.AreEqual(2432902008176640000I , Solution.Problem74.factorial 20I)
+
+[<Test>]
+let factorialDigitSum_number_returnsSum () =
+    Assert.AreEqual(145, Solution.Problem74.factorialDigitSum 145)
+
+[<Test>]
+let factorialDigitSumLoopLength_69_returnsCount () =
+    Assert.AreEqual(5 , Solution.Problem74.factorialDigitSumLoopLength 69)
+
+[<Test>]
+let factorialDigitSumLoopLength_78_returnsCount () =
+    Assert.AreEqual(4 , Solution.Problem74.factorialDigitSumLoopLength 78)
+
+[<Test>]
+let factorialDigitSumLoopLength_145_returnsCount () =
+    Assert.AreEqual(1 , Solution.Problem74.factorialDigitSumLoopLength 145)
+
+[<Test>]
+let ``Digit factorial chains: solution`` () =
+    let result = Solution.Problem74.solution ()
+    Assert.AreEqual(402 , result)
