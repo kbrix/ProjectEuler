dbb20e56be86f07c1740a611e62976aa81b1ccc4
kbrix <kbrix@protonmail.com>
2022-04-03 09:44:11 +0200

    Test caching results from recursive funtion (does not increase speed here)

--- SolutionFS/Problem92.fs
+++ SolutionFS/Problem92.fs
@@ -2,6 +2,21 @@
 
 module Problem92 =
 
+    let memoize f =
+        let cache = System.Collections.Concurrent.ConcurrentDictionary<int,int>()
+        fun x ->
+            cache.GetOrAdd(x, fun x -> f x)
+
+    // This memoization handles recursive functions by defining mutually recursive functions 
+    // g : 'a -> 'b and h : ('a -> 'b) -> 'a -> 'b, where g is the memoized form of f that will get passed to f itself,
+    // and h does the actual work.
+    let memoizeRec f =
+      let mem = System.Collections.Concurrent.ConcurrentDictionary<int,int>();
+      let rec g key = h g key
+      and h r key =
+        mem.GetOrAdd(key, fun key -> f g key)
+      g
+
     let squares i =
         match i with
         | 0 -> 0
@@ -27,8 +42,16 @@ module Problem92 =
         | 89 -> 89
         | _  -> n |> sumDigitSquares |> digitChain
 
+    // Extended version, allowing memoized recursion by passing a memoized version of itself
+    let digitChainRec r (n : int) =
+        match n with 
+        | 1  -> 1
+        | 89 -> 89
+        | _  -> n |> sumDigitSquares |> r
+
     let solution =
+        let memoizedDigitChain = memoizeRec digitChainRec
         [| 1 .. 10_000_000|]
-        |> Array.Parallel.map digitChain
+        |> Array.Parallel.map memoizedDigitChain
         |> Array.filter (fun n -> n = 89)
         |> Array.length
